3e1ee043186d55e64e6d524427961bce39d5105c
josue lobo <darksiders20005@gmail.com>
2021-06-22 15:26:01 -0300

    requisito 1 ok

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let test = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    if (typeof (array[i]) !== 'number') {
+      return undefined;
+    }
+    test += array[i];
+  }
+  return Math.round(test / array.length);
+};
 
 module.exports = average;
